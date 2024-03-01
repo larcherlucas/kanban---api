068f2afc1f4990505a4033884357699f2605df7f
FabienTavernier <fabien.tavernier@oclock.io>
2024-03-01 14:04:29 +0100

    feat(lists): controller

---
+++ src/controllers/list.controller.js
@@ -0,0 +1,27 @@
+import { List } from "../models/list.model.js";
+
+async function getAll(req, res) {
+  try {
+    // 1. je récupère toutes les listes depuis la BDD
+    const lists = await List.findAll({
+      order: [
+        ['position', 'ASC'],
+        ['created_at', 'DESC'],
+      ],
+    });
+
+    // 2. je renvoie les listes au client au format JSON
+    res.json(lists);
+  } catch (error) {
+    console.error(error);
+
+    // 3. en cas d'erreur, je retourne une 500
+    res.status(500).json({
+      error: "Unexpected server error. Please try again later."
+    });
+  }
+}
+
+export {
+  getAll, // sucre syntaxique de `getAll: getAll,`
+};
